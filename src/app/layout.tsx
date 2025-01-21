fb174b0a2e8c8ffbfa42e11539c5efb02ac0ef73
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-21 23:31:20 +0000

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,30 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+            {children}
+          </main>
+
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://github.com/nkrumahthis/invoice-now"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://nkrumahsarpong.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              author: nkrumahsarpong
+            </a>
+          </footer>
+        </div>
       </body>
     </html>
   );
