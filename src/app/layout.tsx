123e3237ac3e832a370e6909dab10541552c06a2
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-22 16:30:57 +0000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -54,6 +55,7 @@ export default function RootLayout({
             </a>
           </footer>
         </div>
+        <Analytics />
       </body>
     </html>
   );
